bc4210a275e4910072d678346807bec8f3c75932
Talha Shahid <talha@earnflex.com>
2025-12-12 12:46:55 +0500

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
 from .config import Config
 from .routes.api import api_bp
@@ -6,6 +7,11 @@ import logging
 
 def create_app(config_name='default'):
     app = Flask(__name__)
+
+    # Enable CORS
+    # CORS(app, origins=["http://127.0.0.1:5500"])
+    CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
+
 
     # Load configuration
     config_class = Config.get(config_name)
